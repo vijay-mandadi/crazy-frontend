c980f9c71a8d945e2f587328a9df4e7099cf4a86
vijay-mandadi <vijaymandadi26@gmail.com>
2024-01-05 12:16:02 +0000

    crazy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-container">
+      <div className="row-1">
+        <div className="container-1">A</div>
+        <div className="container-1">B</div>
+      </div>
+      <div className="row-1">
+        <div className="container-2">C</div>
+        <div className="container-3">D</div>
+      </div>
+      <div className="container-4">E</div>
     </div>
   );
 }
